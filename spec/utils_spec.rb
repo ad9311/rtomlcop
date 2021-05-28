6bf1e6b6b2144fce7b5e41d4afb645be09101e27
Ángel Díaz <diazangel9311@gmail.com>
2021-05-28 10:07:11 -0500

    Fix rspec issues with deleted method

--- spec/utils_spec.rb
+++ spec/utils_spec.rb
@@ -6,19 +6,18 @@ describe Utils do
   let(:toml_file) { Toml::File.new }
   before 'Add new line to toml_file instance' do
     toml_file.line_to_arr('int = 10t0 #bad comment')
-    Utils::Slice.slice_value(toml_file)
     Utils::Slice.get_comment(toml_file)
-    Utils::Slice.get_var_name(toml_file)
+    Utils::Slice.join_var_val(toml_file)
   end
   describe Utils::Slice do
     it 'Return value from line' do
       expect(toml_file.value_arr[0]).to eq('10t0')
     end
+    it 'Returns the current statement without comments' do
+      expect(toml_file.value_arr[2]).to eq('int = 10t0')
+    end
     it 'Returns the comment without the #' do
       expect(toml_file.value_arr[1]).to eq('bad comment')
-    end
-    it 'Returns the variable name' do
-      expect(toml_file.value_arr[2]).to include('int')
     end
     it 'Returns the bad character in the inter value' do
       expect(Utils::Slice.get_bad_int(toml_file)).to eq('t')
