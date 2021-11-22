5db423644c3dc4bfe0dca07f22e71b0406a986a0
Ángel Díaz <diazangel9311@gmail.com>
2021-11-21 21:19:30 -0500

    Improve offences collection

--- lib/table_key/table_type.rb
+++ lib/table_key/table_type.rb
@@ -1,28 +1,36 @@
 require_relative '../utils/codes'
 require_relative './array_table_collection'
 require_relative '../offence/major_offence'
+require_relative '../offence/minor_offence'
 
 class TableType
   include Codes::Status
   include Codes::Offence
 
   def initialize
+    @lnum = nil
     @offences = []
     @arr_tbl_li = []
-    @tbl_li =[]
+    @tbl_li = []
   end
 
   def insp_table(line)
+    tbl_set(line)
     switch_tbl_type(line)
+    send_resp
   end
 
   private
 
+  def tbl_set(line)
+    @lnum = line.fetch(:lnum)
+    @offences = []
+  end
+
   def switch_tbl_type(line)
     case count_brakets(line)
-    when true 
-      nest = insp_nesting(line)
-      [*nest]
+    when true
+      insp_nesting(line)
     when false
       [TES]
     else
@@ -30,11 +38,17 @@ class TableType
     end
   end
 
+  def send_resp
+    return OK if @offences.empty?
+
+    @offences
+  end
+
   def count_brakets(line)
     tbl = line.fetch(:table)
-    return OK if tbl.nil?
+    return true if tbl.nil?
 
-    brakets = tbl.count("[]")
+    brakets = tbl.count('[]')
     tbl_major_offnc(UNCL_TBL) if brakets.odd?
     tbl_major_offnc(EXT_BRK) if brakets > 4
 
@@ -49,10 +63,18 @@ class TableType
 
   def insp_nesting(line)
     unless line.fetch(:table).nil?
+      resps = []
       @arr_tbl_li << ArrayTableCollection.new(line)
-      @offences << @arr_tbl_li.last.insp_collection(@arr_tbl_li)
+      resps << @arr_tbl_li.last.insp_collection(@arr_tbl_li)
+      apnd_off(resps)
     end
     @arr_tbl_li.last.add_child(line) unless line.fetch(:key).nil?
     @offences
+  end
+
+  def apnd_off(resps)
+    resps.each do |resp|
+      @offences << MinorOffence.new(@lnum, resp) unless resp.nil? || resp == OK
+    end
   end
 end
